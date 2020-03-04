e93d960bb7835f9990d68afce61e7e5dd263f776
Pandanym <nympanda@gmail.com>
2020-03-04 20:55:42 +0100

    Setup for entity behaviours

---
+++ Assets/Scripts/Scriptable/Ability.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ability", menuName = "ScriptableObjects/Ability", order = 1)]
+/// <summary>
+/// Holds all of the data regarding an Ability
+/// </summary>
+public class Ability : ScriptableObject
+{
+    public TileArea castArea;
+    public TileArea effectArea;
+}
--- Assets/Scripts/Scriptable/Entity.cs
+++ Assets/Scripts/Scriptable/Entity.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 /// <summary>
 /// Holds all of the data regarding an Entity
@@ -6,5 +7,9 @@
 [CreateAssetMenu(fileName = "Entity", menuName = "ScriptableObjects/Entity", order = 1)]
 public class Entity : ScriptableObject
 {
-    
+    public string displayName;
+    public int maxHealth;
+    public List<Ability> abilities;
+    public Brain brain;
+    public int alignement;
 }
