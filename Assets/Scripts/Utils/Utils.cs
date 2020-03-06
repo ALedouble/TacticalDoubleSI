db74d87c0d04eb3c62a59ae9879944cc9239ba2e
AlexisLedouble <a.ledouble@rubika-edu.com>
2020-03-06 13:45:10 +0100

    EffectArea working

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public static class Utils 
 {
+    public static bool doContinue;
     public static List<Vector2> vectors = new List<Vector2>(new Vector2[] { new Vector2(0, 1), new Vector2(1, 1), new Vector2(1, 0),
         new Vector2(1, -1), new Vector2(0, -1), new Vector2(-1, -1),new Vector2(-1, 0),new Vector2(-1, 1) });
+
+   
 }
