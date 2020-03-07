b66067f3f6282980099e0f3010d16eecdf9ccbee
AlexisLedouble <a.ledouble@rubika-edu.com>
2020-03-07 17:25:53 +0100

    Push in progress

--- Assets/Scripts/Behaviour/EntityBehaviour.cs
+++ Assets/Scripts/Behaviour/EntityBehaviour.cs
@@ -230,10 +230,15 @@ public class EntityBehaviour : MonoBehaviour
         {
             if (MapManager.GetListOfEntity()[y].GetPosition() == effectPosition)
             {
+                Vector2Int enemyPosition = MapManager.GetListOfEntity()[y].GetPosition();
                 Debug.Log(MapManager.GetListOfEntity()[y].GetPosition());
                 Debug.Log(currentTile.position);
                 Debug.Log((currentTile.position - (castCase + GetPosition())));
                 Debug.Log(CombatUtils.PushEffect(MapManager.GetListOfEntity()[y].GetPosition(), currentTile.position));
+
+                Vector2Int pushVector = CombatUtils.PushEffect(MapManager.GetListOfEntity()[y].GetPosition(), currentTile.position);
+                CombatUtils.Push(MapManager.GetListOfEntity()[y], pushVector);
+
             }
         }
     }
--- Assets/Scripts/Utils/CombatUtils.cs
+++ Assets/Scripts/Utils/CombatUtils.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,6 +14,15 @@ public static class CombatUtils
     public static Vector2Int PushEffect(Vector2Int enemyPosition, Vector2Int myPosition)
     {
         Vector2Int pushVector = enemyPosition - myPosition;
-        return myPosition + (pushVector*2);
+        return myPosition + (pushVector* 2);
+    }
+
+    public static Sequence Push(EntityBehaviour entity, Vector2Int pushVector)
+    {
+        Sequence pushSequence = DOTween.Sequence();
+        Ease pushEase = Ease.InQuad;
+
+        pushSequence.Append(entity.transform.DOMove(new Vector3(pushVector.x, 0, pushVector.y), 0.5f));
+        return pushSequence;
     }
 }
