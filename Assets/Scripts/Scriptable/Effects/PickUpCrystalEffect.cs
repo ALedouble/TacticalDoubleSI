99b1750e2567944f50dbb1b97e57b00e3e9a3b12
AlexisLedouble <a.ledouble@rubika-edu.com>
2020-03-09 20:11:03 +0100

    PickUpEffect [WIP]

---
+++ Assets/Scripts/Scriptable/Effects/PickUpCrystalEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpCrystalEffect : AbilityEffect
+{
+    // Start is called before the first frame update
+    public override void Activate(EntityBehaviour entity, Ability ability, TileData castTile)
+    {
+        ApplyEffect(entity, ability, castTile, (x) => {
+            if (x.data.alignement == Alignement.Neutral)
+            {
+                PickUp(entity);
+            }
+        });
+    }
+
+    // Update is called once per frame
+    public void PickUp(EntityBehaviour entity)
+    {
+        
+    }
+}
