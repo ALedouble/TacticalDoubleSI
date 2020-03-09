db85fc5589e572020b1ed6f7b6928a6c38f114b6
Pandanym <nympanda@gmail.com>
2020-03-09 21:35:29 +0100

    progression system start

---
+++ Assets/Scripts/Scriptable/EntityProgression.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EntityProgression", menuName = "ScriptableObjects/EntityProgression")]
+public class EntityProgression : ScriptableObject
+{
+    public float healthIncrement;
+    public float actionPointsIncrement;
+    public float armorIncrement;
+
+    public List<AbilityProgression> abilityProgression = new List<AbilityProgression>();
+}
