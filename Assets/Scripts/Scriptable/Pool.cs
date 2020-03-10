61bc9bef9cb1135208f626350588838aef6fc02e
Pandanym <nympanda@gmail.com>
2020-03-10 16:37:49 +0100

    fx inte

---
+++ Assets/Scripts/Scriptable/Pool.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Pool", menuName = "Pooling/Pool")]
+public class Pool : ScriptableObject
+{
+    public GameObject prefab;
+    public int size;
+
+    [HideInInspector] public bool renaming;
+
+    public List<GameObject> inactiveObjects = new List<GameObject>();
+    public List<GameObject> activeObjects = new List<GameObject>();
+
+    public GameObject GetObject()
+    {
+        GameObject obj = null;
+        
+        if (inactiveObjects.Count < 1)
+        {
+            obj = Instantiate(prefab);
+            activeObjects.Add(obj);
+
+            return obj;
+        }
+        else
+        {
+            obj = inactiveObjects[0];
+            obj.transform.rotation = prefab.transform.rotation;
+
+            inactiveObjects.Remove(obj);
+            activeObjects.Add(obj);
+        }
+        
+        return obj;
+    }
+}
