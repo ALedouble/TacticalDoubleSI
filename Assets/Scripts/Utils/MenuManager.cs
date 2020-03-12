123eee46f051e6c0f3f97134fdc98c99bb2e8e1d
AlexisLedouble <a.ledouble@rubika-edu.com>
2020-03-12 11:46:33 +0100

    Menu [WIP]

---
+++ Assets/Scripts/Utils/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+
+    Tween squishTween;
+   
+    public void Squish(GameObject button)
+    {
+        squishTween?.Kill(true);
+        squishTween = button.transform.DOPunchScale(Quaternion.AngleAxis(-45, Vector3.up) * new Vector3(-.2f, .2f, 0), .2f, 15, 1f);
+    }
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
